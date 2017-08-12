adb2a3b4a04e9c3f567516d708395f86dad71429
nagaraju_y <nagaraju_y@NagarajuYasarapu>
2017-08-13 01:25:11 +0530

    Camera functionality has been added and verified..

--- src/js/viewModels/camera.js
+++ src/js/viewModels/camera.js
@@ -5,17 +5,20 @@
 /*
  * Your camera ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojbutton'],
-    function (oj, ko, $, app) {
+define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'util/commonhelper', 'util/imageSavingUtil', 'ojs/ojbutton'],
+    function (oj, ko, $, app, commonUtil, imgUtil) {
 
         function CameraViewModel(params) {
             var self = this;
 
             // Header Config
             self.headerConfig = {'viewName': 'header', 'viewModelFactory': app.getHeaderModel()};
-            
-            self.capturePhoto = function() {
-                console.log("Have to capture photo");
+
+            self.capturePhoto = function () {
+                navigator.camera.getPicture(imgUtil.onCaptureSuccess, imgUtil.onCaptureFail, {
+                    quality: 50,
+                    destinationType: Camera.DestinationType.FILE_URI
+                });
             };
         }
 
